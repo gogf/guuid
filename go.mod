7627fd486ab7b24f29b0e9e5156c7499f3deb9f3
john <john@johng.cn>
2020-05-16 15:31:05 +0800

    move package guuid from gf to a standalone package

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gogf/guuid
+
+go 1.11
+
+require (
+	github.com/gogf/gf v1.12.3
+	github.com/google/uuid v1.1.1
+)
---
+++ guuid_z_bench_test.go
@@ -0,0 +1,72 @@
+// Copyright 2019 gf Author(https://github.com/gogf/gf). All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file,
+// You can obtain one at https://github.com/gogf/gf.
+
+// go test *.go -bench=".*" -benchmem
+
+package guuid_test
+
+import (
+	"github.com/gogf/guuid"
+	"testing"
+)
+
+func Benchmark_New(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		guuid.New().String()
+	}
+}
+
+func Benchmark_NewUUID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if u, err := guuid.NewUUID(); err != nil {
+			panic(err)
+		} else {
+			u.String()
+		}
+	}
+}
+
+func Benchmark_NewDCEGroup(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if u, err := guuid.NewDCEGroup(); err != nil {
+			panic(err)
+		} else {
+			u.String()
+		}
+	}
+}
+
+func Benchmark_NewDCEPerson(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if u, err := guuid.NewDCEPerson(); err != nil {
+			panic(err)
+		} else {
+			u.String()
+		}
+	}
+}
+
+func Benchmark_NewRandom(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if u, err := guuid.NewRandom(); err != nil {
+			panic(err)
+		} else {
+			u.String()
+		}
+	}
+}
+
+func Benchmark_NewMD5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		guuid.NewMD5(guuid.UUID{}, []byte("")).String()
+	}
+}
+
+func Benchmark_NewSHA1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		guuid.NewSHA1(guuid.UUID{}, []byte("")).String()
+	}
+}
